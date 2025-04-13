53732b9f0595554e855e5ac873ff86370e350c40
asthaade <adeastha398>
2025-04-13 10:50:37 +0530

    added accept/reject feature

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,11 +2,24 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BASE_URL } from '../utils/constants';
-import { addRequests } from '../utils/requestSlice';
+import { addRequests, removeRequest } from '../utils/requestSlice';
 
 const Requests = () => {
     const requests = useSelector((store) => store.requests);
     const dispatch = useDispatch();
+
+    const reviewRequest = async (status, _id) => {
+        try {
+            const res = axios.post(
+            BASE_URL + "/request/review/" + status + "/" + _id,
+            {},
+            { withCredentials: true }
+        );
+            dispatch(removeRequest(_id));
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     const fetchRequests = async () => {
         try{
@@ -50,13 +63,12 @@ const Requests = () => {
                         {age && gender && <p>{age + ", " + gender}</p>}
                         <p>{about}</p>
                     </div>
-                    <div >
+                    
                     <div>
-                        <button className="btn btn-primary mx-2">Reject</button>
-                        <button className="btn btn-secondary mx-2">Accept</button>
+                        <button className="btn btn-primary mx-2" onClick={() => reviewRequest('rejected',request._id)}>Reject</button>
+                        <button className="btn btn-secondary mx-2"onClick={() => reviewRequest('accepted',request._id)}>Accept</button>
                 </div>
                 </div>
-                    </div>
                 );
             })}
         </div>
